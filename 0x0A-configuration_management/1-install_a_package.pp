2c1ab13e7fe7bf5b65a8c5985e7b762ba4986769
Jukunye <jukunyes@gmail.com>
2023-11-24 21:13:31 +0300

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+# install puppet-lint using Puppet
+
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
+}
