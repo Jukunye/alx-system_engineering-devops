4f72e7fe4752c1e628949a39ba171c6e9629b6bc
Jukunye <jukunyes@gmail.com>
2023-11-26 11:34:55 +0300

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #!/usr/bin/pup
 # Install flask
-package {'flask':
+package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
