df94014b2e8750740beed3ba7934ef123764b4e5
Jukunye <jukunyes@gmail.com>
2023-11-29 01:00:07 +0300

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,6 +4,13 @@ package { 'nginx':
   ensure => 'installed',
 }
 
+file_line { 'redirect_me':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
 file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
