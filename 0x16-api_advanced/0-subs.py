6892516b43d99bf73a32b19f146dd16b9c23f1ba
Jukunye <jukunyes@gmail.com>
2024-01-16 21:24:30 +0300

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Contains a function that queries the Reddit API and
+returns the number of subscribers (not active users, total subscribers)
+for a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function that queries the Reddit API"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    # Setting up a custom User-Agent to avoid too many Request errors
+    headers = {'User-Agent': 'Google Chrome Version 120.0.6099.217'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()["data"]["subscribers"]
+        return data
+    return 0
