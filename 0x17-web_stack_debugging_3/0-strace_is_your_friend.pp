1f171d5164cde46fae3b57be3ccbc771e78f7e82
Jukunye <jukunyes@gmail.com>
2024-01-17 21:30:44 +0300

    typo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,6 +3,6 @@
 $file_check = '/var/www/html/wp-settings.php'
 
 exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  command => "sed -i 's/phpp/php/g' ${file_check}",
   path    => ['/bin','/usr/bin']
 }
